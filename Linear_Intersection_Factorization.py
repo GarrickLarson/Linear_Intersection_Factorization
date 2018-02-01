96ac145c271c0f5776e46348ba8d7ba4850a2ef6
Garrick Larson <glarson6@gatech.edu>
2018-02-01 02:00:58 -0500

    Prettied things up

--- Linear_Intersection_Factorization.py
+++ Linear_Intersection_Factorization.py
@@ -3,45 +3,53 @@ import time
 
 # Garrick Larson
 # 2018/2/1 0121 hrs
-
+# prime factors were downloaded from http://www.primos.mat.br/
 f1 = 999999999959
-f2 = 999939472397
+f2 = 999875882263
 
 cp = f1 * f2
-print(f1)
-print(f2)
-print(cp)
+print(' Factor 1: ' + str(f1))
+print(' Factor 2: ' + str(f2))
+print('Composite: ' + str(cp))
 
 
 # -x intercept method
+print()
+print('(((Linear Intersection Method)))')
+print()
 start_time = time.time()
 count = 0
 d = math.floor(math.sqrt(cp)*2)+1
 
 while True:
-    count += 1
-    # loop_time = time.time()
+    # count += 1  # comment out count to increase speed
     interceptor = math.floor(.5 * (d - math.sqrt(d**2 - 4*cp)))
     if cp % interceptor == 0:
         break
     d += 1
-    # print(loop_time-time.time())
 
 print("Iterations: " + str(count))
 t0 = time.time() - start_time
 print("--- %s seconds ---" % (t0))
-print(interceptor)
-print(cp/interceptor)
+print('Factors: ' + str(interceptor) + ', ' + str(cp/interceptor))
 
 
 
 # brute force 
-b = math.floor(math.sqrt(cp))
+print()
+print('(((Brute Force Method)))')
+print()
 start_time = time.time()
+count = 0
+b = math.floor(math.sqrt(cp))
+
 while cp % b != 0:
     b -= 1
+    # count += 1  # comment out count to increase speed
 t1 = time.time() - start_time
 print(b)
+print("Iterations: " + str(count))
 print("--- %s seconds ---" % (t1))
-
-print('Ratio: ' + str(t1/t0))
+print('Factors: ' + str(b) + ', ' + str(cp/b))
+print()
+print('Ratio: ' + str(t1/t0) + ' times faster than brute force')
